8f8522fca327ffd6606ad4c345d4c0a848022fd0
Stenio Joseph <steniojoseph@hotmail.com>
2024-07-29 23:02:02 -0400

    feat: - Refactor Program.cs, remove the services configuration, and middleware move them to the Startup.cs class. - Add System.ComponentModel.Annotations package - Add Microsoft.Extensions.Hosting

---
+++ src/MyPortfolioProject.Presentation/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace MyPortfolioProject.Presentation
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;  }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyPortfolioProject.Presentation v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler(exceptionHandlerApp =>
+                {
+                    exceptionHandlerApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                        var exception = exceptionHandlerPathFeature?.Error;
+
+                        var problemDetails = new ProblemDetails
+                        {
+                            Status = StatusCodes.Status500InternalServerError,
+                            Title = "An error occurred while processing your request.",
+                            Detail = exception?.Message
+                        };
+
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
+                    });
+                });
+
+                app.UseHsts();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
